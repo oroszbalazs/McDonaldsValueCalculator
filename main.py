d9466033459544fa5e1d7e3a3f167806389c0869
Orosz Balázs <orosz.balazs100@gmail.com>
2021-08-01 18:15:51 +0200

    Get prices from NetPincer

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import sys
 import requests
 from bs4 import BeautifulSoup
+from tabulate import tabulate
 
 # first sandwiches
 # 1. get all the URLs for the sandwiches
@@ -8,6 +10,17 @@ from bs4 import BeautifulSoup
 
 # have to check netpincer for the prices
 
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
 
 def get_product_category_by_name(name: str):
     if name == 'szendvicsek-es-wrapek':
@@ -15,33 +28,53 @@ def get_product_category_by_name(name: str):
 
 
 def get_products_html():
+    print(bcolors.OKBLUE + 'https://www.mcdonalds.hu/termekek/ HTML lekérése...' + bcolors.ENDC)
+    print()
     return requests.get('https://www.mcdonalds.hu/termekek/')
 
 
-products_html = get_products_html()
+def get_menus_json():
+    return requests.get('https://hu.fd-api.com/api/v5/vendors/g9oz?include=menus&language_id=3&dynamic_pricing=0&'
+                        'opening_type=delivery&basket_currency=HUF').json()
+
+products_response = get_products_html()
+
+if products_response.status_code != 200:
+    print(bcolors.FAIL + 'A McDonald\'s weboldalát nem sikerült elérni...' + bcolors.ENDC)
+    sys.exit(1)
 
 product_category = get_product_category_by_name('szendvicsek-es-wrapek')
 
-sandwiches_soup = BeautifulSoup(products_html.text, 'html.parser')
+sandwiches_soup = BeautifulSoup(products_response.text, 'html.parser')
 
-products = sandwiches_soup.find_all('div', {'id': product_category})[0].ul
+uls = sandwiches_soup.find_all('div', {'id': product_category})[0].find_all('ul')
 
 products_dict = {}
 
-for child in products:
-    product_page = requests.get('https://www.mcdonalds.hu' + child.article.a['href'])
-    product_soup = BeautifulSoup(product_page.text, 'html.parser')
-    product_name = child.article.a['href'].split('/')[-1]
+for ul in uls:
+    for child in ul:
+        product_page_response = requests.get('https://www.mcdonalds.hu' + child.article.a['href'])
+        product_soup = BeautifulSoup(product_page_response.text, 'html.parser')
+        product_name = child.article.a['href'].split('/')[-1].replace('maestro-', '')
+        if 'glutenmentes' in product_name:
+            continue
 
-    # there's a tr in the thead
-    calorie = product_soup.find_all('table', {'class': 'nutrition-table'})[0].find_all('tr')[2].find_all('td')[1].text
+        # there's a tr in the thead
+        calorie = product_soup.find_all('table', {'class': 'nutrition-table'})[0].find_all('tr')[2].find_all('td')[1].text
 
-    products_dict[product_name] = calorie
+        products_dict[product_name] = {'calorie': calorie}
 
 # you can sort dicts starting from Python 3.7+
-sorted_calories = {k: v for k, v in sorted(products_dict.items(), key=lambda item: item[1], reverse=True)}
+sorted_calories = {k: v for k, v in sorted(products_dict.items(), key=lambda item: item[1]['calorie'], reverse=True)}
 
-for key, value in sorted_calories.items():
-    print(key, value)
+print(bcolors.OKGREEN + 'A következő adatokat kaptuk: ' + bcolors.ENDC)
 
 
+# now let's get the stuff from netpincer
+
+for item in get_menus_json()['data']['menus'][0]['menu_categories'][2]['products']:
+    product_name = item['name'].replace(' ', '-').lower().replace('®', '')
+    if product_name in sorted_calories:
+        sorted_calories[product_name]['price'] = item['product_variations'][0]['price']
+
+print(tabulate([[key, value] for key, value in sorted_calories.items()], headers=['Termék', 'Kcal']))
