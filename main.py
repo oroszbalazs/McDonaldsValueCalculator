482f54cc7349e3f1124310216a9dcf951782c5d2
Orosz Balázs <orosz.balazs100@gmail.com>
2021-08-09 21:18:45 +0200

    Sort by value and some refactoring

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import requests
 from bs4 import BeautifulSoup
 from tabulate import tabulate
 
+# TODO: do something with products that have different names across netpincer and mcdonalds.hu
+
 # first sandwiches
 # 1. get all the URLs for the sandwiches
 # 2. iterate through them while checking the calories
@@ -37,6 +39,31 @@ def get_menus_json():
     return requests.get('https://hu.fd-api.com/api/v5/vendors/g9oz?include=menus&language_id=3&dynamic_pricing=0&'
                         'opening_type=delivery&basket_currency=HUF').json()
 
+
+def get_calories(products_dict):
+    for ul in uls:
+        for child in ul:
+            product_page_response = requests.get('https://www.mcdonalds.hu' + child.article.a['href'])
+            product_soup = BeautifulSoup(product_page_response.text, 'html.parser')
+            product_name = child.article.a['href'].split('/')[-1].replace('maestro-', '')
+            if 'glutenmentes' in product_name:
+                continue
+
+            # there's a tr in the thead
+            calorie = product_soup.find_all('table', {'class': 'nutrition-table'})[0].find_all('tr')[2].find_all('td')[
+                1].text
+
+            products_dict[product_name] = {'calorie': calorie}
+
+
+def get_prices(products_dict):
+    for item in get_menus_json()['data']['menus'][0]['menu_categories'][2]['products']:
+        product_name = item['name'].replace(' ', '-').lower().replace('®', '')
+        if product_name in products_dict:
+            products_dict[product_name]['price'] = item['product_variations'][0]['price']
+            products_dict[product_name]['value'] = int(products_dict[product_name]['calorie']) / \
+                                                   int(products_dict[product_name]['price'])
+
 products_response = get_products_html()
 
 if products_response.status_code != 200:
@@ -51,30 +78,16 @@ uls = sandwiches_soup.find_all('div', {'id': product_category})[0].find_all('ul'
 
 products_dict = {}
 
-for ul in uls:
-    for child in ul:
-        product_page_response = requests.get('https://www.mcdonalds.hu' + child.article.a['href'])
-        product_soup = BeautifulSoup(product_page_response.text, 'html.parser')
-        product_name = child.article.a['href'].split('/')[-1].replace('maestro-', '')
-        if 'glutenmentes' in product_name:
-            continue
-
-        # there's a tr in the thead
-        calorie = product_soup.find_all('table', {'class': 'nutrition-table'})[0].find_all('tr')[2].find_all('td')[1].text
-
-        products_dict[product_name] = {'calorie': calorie}
-
-# you can sort dicts starting from Python 3.7+
-sorted_calories = {k: v for k, v in sorted(products_dict.items(), key=lambda item: item[1]['calorie'], reverse=True)}
+get_calories(products_dict)
 
 print(bcolors.OKGREEN + 'A következő adatokat kaptuk: ' + bcolors.ENDC)
 
 
 # now let's get the stuff from netpincer
+get_prices(products_dict)
 
-for item in get_menus_json()['data']['menus'][0]['menu_categories'][2]['products']:
-    product_name = item['name'].replace(' ', '-').lower().replace('®', '')
-    if product_name in sorted_calories:
-        sorted_calories[product_name]['price'] = item['product_variations'][0]['price']
 
-print(tabulate([[key, value] for key, value in sorted_calories.items()], headers=['Termék', 'Kcal']))
+# you can sort dicts starting from Python 3.7+
+sorted_calories = {k: v for k, v in sorted(filter(lambda x: 'price' in x[1], products_dict.items()), key=lambda item: item[1]['value'], reverse=True)}
+
+print(tabulate([[key, value['calorie'], value['price'], value['value']] for key, value in sorted_calories.items() if 'price' in value], headers=['Termék', 'Kcal', 'Price', 'Value']))
